51bf2964c943c84e549ecd22b4a0f212c0d71d75
ivesfg1 <wigzfurtado@gmail.com>
2023-02-01 21:07:31 -0300

    mudando pra voltar a usar objectid ao inves de int

--- database.py
+++ database.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient, TEXT
 from models import AnnotationModel, AnnotationPatchModel
 from fastapi import HTTPException, status
 
+from bson import ObjectId
+
 client = MongoClient()
 
 database = client["annotation_app_python"]
@@ -24,26 +26,16 @@ collection.create_index(
 )
 
 
-last_id_used = 0
-
-
-def generate_integer_id():
-    global last_id_used
-    last_id_used += 1
-    return last_id_used - 1
-
-
 def fetch_all(query=None):
     result = collection.find()
     if query:
-        result = collection.find(    
-            {'$text': {'$search': query}},     
-            {'score': {'$meta': 'textScore'}}
-        ).sort([('score', {'$meta': 'textScore'})])
+        result = collection.find(
+            {"$text": {"$search": query}}, {"score": {"$meta": "textScore"}}
+        ).sort([("score", {"$meta": "textScore"})])
     return list(result)
 
 
-def fetch_one(annotation_id: int):
+def fetch_one(annotation_id: ObjectId):
     result = collection.find_one({"_id": annotation_id})
     if result is not None:
         return result
@@ -51,15 +43,12 @@ def fetch_one(annotation_id: int):
 
 
 def create(annotation: AnnotationModel):
-    new_annotation_id = collection.insert_one(
-        {"_id": generate_integer_id(), **annotation.dict()}
-    ).inserted_id
+    new_annotation_id = collection.insert_one({**annotation.dict()}).inserted_id
     result = collection.find_one({"_id": new_annotation_id})
-    print(result)
     return result
 
 
-def update(annotation_id: int, annotation: AnnotationPatchModel):
+def update(annotation_id: ObjectId, annotation: AnnotationPatchModel):
     result = collection.find_one_and_update(
         {"_id": annotation_id}, {"$set": {**annotation.dict(exclude_unset=True)}}
     )
@@ -68,7 +57,7 @@ def update(annotation_id: int, annotation: AnnotationPatchModel):
     raise NOT_FOUND_EXCEPTION
 
 
-def delete(annotation_id: int):
+def delete(annotation_id: ObjectId):
     success_message = {"message": "annotation successfuly deleted"}
     result = collection.find_one_and_delete({"_id": annotation_id})
     if result is not None:
